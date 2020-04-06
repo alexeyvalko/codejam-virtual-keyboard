bae1fe4705287bb119b9e6eed4d9ea0d3e9a53ff
Alexey Valko <valko.alexey@gmail.com>
2020-04-06 20:46:37 +0300

    refactor: format code

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ function render() {
     'ControlLeft', 'AltLeft', 'Win', ' ', 'AltRight', 'ControlRight', '←', '↑', '↓', '→',
   ];
 
-  const pressShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace','Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', 'Del',
-  ];
+  const pressShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', 'Del'];
   document.body.insertAdjacentHTML('afterbegin', '<div class="megatext"> Переключение языка Ctrl + Alt</div>');
   document.body.insertAdjacentHTML('afterbegin', '<div class="keyboard" id="keyboard"> </div>');
   document.body.insertAdjacentHTML('afterbegin', '<textarea class="input" id="input"></textarea>');
